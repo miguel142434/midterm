1d034546d822512429536b86157deff59a1a9f3a
miguel142434 <miguelzuleta2020@gmail.com>
2025-03-28 16:47:25 -0500

    Subiendo los archivos

--- src/auth/auth.guard.ts
+++ src/auth/auth.guard.ts
@@ -5,7 +5,7 @@ import { Dictator } from 'src/dictators/entities/dictator.entity';
 
 @Injectable()
 export class AutenticadorGuard implements CanActivate {
-  // Para poder buscar si el nombre del dictador existe
+
   constructor(
     @InjectRepository(Dictator)
     private dictatorRepository: Repository<Dictator>,
@@ -16,15 +16,15 @@ export class AutenticadorGuard implements CanActivate {
     const req=context.switchToHttp().getRequest();
     const user=req.user;
 
-    // Verifica si hay un usuario y un username en el token
+
     if (!user || !user.name) {
       throw new UnauthorizedException('No estás autenticado o falta el nombre de usuario');
     }
 
-    // Busca el dictator por name en la base de datos
+
     const dictator = await this.dictatorRepository.findOne({ where: { name: user.name }, });
 
-    // Si no se encuentra el dictator, deniega acceso
+
     if (!dictator) {
       throw new UnauthorizedException('No eres un dictator registrado');
     }
