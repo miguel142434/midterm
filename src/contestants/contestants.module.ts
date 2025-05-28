4f68fe823ae9b5dbcbe5ea07abbf263271b7631b
miguel142434 <miguelzuleta2020@gmail.com>
2025-05-27 22:03:52 -0500

    modulo

--- src/contestants/contestants.module.ts
+++ src/contestants/contestants.module.ts
@@ -3,11 +3,14 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { ContestantsService } from './contestants.service';
 import { ContestantsController } from './contestants.controller';
 import { Contestant } from './entities/contestant.entity';
+import { Dictator } from 'src/dictators/entities/dictator.entity';
 
 @Module({
-    imports: [TypeOrmModule.forFeature([Contestant])],
-    controllers: [ContestantsController],
-    providers: [ContestantsService],
-    exports: [ContestantsService],
+  imports: [
+    TypeOrmModule.forFeature([Contestant, Dictator]),
+  ],
+  controllers: [ContestantsController],
+  providers: [ContestantsService],
+  exports: [ContestantsService],
 })
 export class ContestantsModule {}
